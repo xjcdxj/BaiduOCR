0fac0fb90034e5c16aa408f4780a0a3989879c2d
xjcdxj <yuxjc@outlook.com>
2019-10-10 09:00:40 +0800

    Revert "Add unrecognized text prompts"
    This reverts commit 29959566

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from window import Ui_MainWindow
-from base import ocr_token
-from PyQt5 import QtCore, QtGui, QtWidgets
-import sys, json, base64
-from urllib import request, parse
-from threading import Thread
+import base64
+import json
 import re
+import sys
+from threading import Thread
+from urllib import request, parse
+from PyQt5 import QtCore, QtWidgets
+from base import ocr_token
+from window import Ui_MainWindow
 
 
 class App(QtWidgets.QMainWindow, Ui_MainWindow):
-    signal = QtCore.pyqtSignal(str, str)
+    signal = QtCore.pyqtSignal(str)
 
     def __init__(self):
         super(App, self).__init__()
@@ -28,18 +30,14 @@ class App(QtWidgets.QMainWindow, Ui_MainWindow):
         self.image = QtWidgets.QFileDialog.getOpenFileName(self, '.')[0]
         self.lineEdit.setText(self.image)
 
-    def warn(self, status, info):
-        if status == 'information':
-            self.textBrowser.setText(info)
-        elif status == 'warning':
-            QtWidgets.QMessageBox.information(self, '提示', info, QtWidgets.QMessageBox.Ok)
+    def warn(self, info):
+        self.textBrowser.setText(info)
 
     def parse(self):
         sender = self.sender()
         if sender == self.start_button:
-            if not re.match(r'.+\.(jpg|png)', self.lineEdit.text()):
+            if re.match(r'.+/.+\.(jpg)|(png)', self.lineEdit.text()):
                 QtWidgets.QMessageBox.warning(self, '警告', '图片错误', QtWidgets.QMessageBox.Ok)
-
             else:
                 t = Thread(target=self.start, args=())
                 t.start()
@@ -48,7 +46,7 @@ class App(QtWidgets.QMainWindow, Ui_MainWindow):
             self.choose_img()
 
     def start(self):
-        self.signal.emit('information', '识别中。。。')
+        self.signal.emit('识别中。。。')
         token = ocr_token()
         url = 'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic'
         with open(self.image, 'rb') as f:
@@ -67,16 +65,13 @@ class App(QtWidgets.QMainWindow, Ui_MainWindow):
         result = response.read().decode()
         try:
             result = json.loads(result)['words_result']
-            if not result:
-                self.signal.emit('warning', '未识别出文字')
-            else:
-                self.signal.emit('information', '')
-                text = ''
-                for i in result:
-                    text = text + i['words'] + '\n'
-                self.signal.emit('information', text)
+            self.signal.emit('')
+            text = ''
+            for i in result:
+                text = text + i['words'] + '\n'
+            self.signal.emit(text)
         except KeyError:
-            self.signal.emit('warning', '识别失败！！！')
+            self.signal.emit('失败！！！')
 
 
 if __name__ == '__main__':
